79ce0ff6d0fa30e69de75eca192f0d16bd602e23
Kuan Butts <kuanbutts@gmail.com>
2018-01-08 18:42:32 -0800

    faster

--- js/main.js
+++ js/main.js
@@ -1,43 +1,9 @@
 function init() {
-	var scene = new THREE.Scene();
-	var gui = new dat.GUI();
-	var gridSize = 14;
-	//grid is a global variable
+	var gridSize = 14;	
+
+	// Grid is a global variable
 	grid = geoGrid(gridSize);
-	
-	var min = 2;
-	var max = gridSize-2;
-	var randomValues = parseInt(getRandomInt(1, (Math.pow(gridSize,3)))/4);
-	var pointLight = getPtLight(1);
-	var pointLight2 = getPtLight(1);
-
-	var guiFunctions = {
-		MinimumSpread: 2,
-		MaximumSpread: Math.cbrt(grid.children.length)-2,
-		Size: gridSize,
-		Random: randomValues,
-		Iterate: function(){
-			testPt(grid);
-			},
-		ClearCanvas: function(){
-			scene.remove(grid);
-			},
-		Populate: function(){
-			var size = guiFunctions.Size;
-			grid = geoGrid(size); 
-			scene.add(grid);
-			var randomValues = guiFunctions.Random;
-			randStartCoord(this.MinimumSpread, this.MaximumSpread, randomValues, grid);
-			colorGeo(grid);
-			}
-		};
-
-	pointLight.position.y = 60;
-	pointLight.position.x = 60;
-	pointLight.position.z = 60;
-	pointLight2.position.y = -60;
-	pointLight2.position.x = -60;
-	pointLight2.position.z = -60;
+	scene = new THREE.Scene();
 
 	var camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 1, 1000);
 	camera.position.z = 25;
@@ -51,64 +17,55 @@ function init() {
 
 	var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	scene.add(grid);
-	scene.add(pointLight);
-	scene.add(pointLight2);
+	var randomValues = parseInt(getRandomInt(1, (Math.pow(gridSize,3)))/4);
+	randStartCoord(2, gridSize - 2, randomValues, grid);
 
-	randStartCoord(min, max, randomValues, grid);
-	colorGeo(grid);
-
+	// TODO: Understand what is being updated and why...
 	update(renderer, scene, camera, controls);
 
-	gui.add(guiFunctions, 'Iterate');
-	gui.add(guiFunctions, 'ClearCanvas');
-	gui.add(guiFunctions, 'Populate');
-	gui.add(guiFunctions, 'Random', 1, grid.children.length).step(1);
-	gui.add(guiFunctions, 'MinimumSpread', 0, Math.cbrt(grid.children.length)).step(1);
-	gui.add(guiFunctions, 'MaximumSpread', 0, Math.cbrt(grid.children.length)).step(1);
-	gui.add(guiFunctions, 'Size', 3, 16).step(1);
-
 	return scene;
-	};
+};
 
 function getRandomInt(min, max){
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
-	}
+}
 
-function geoGrid(gridSize){
-	var group = new THREE.Group();
-	group.name = '3D_GeoGrid';
+function geoGrid(gridSize) {
+	gridList = []
 	for (var i = 0; i < gridSize; i++){
 		for (var m = 0; m < gridSize; m++){
 			for (var n = 0; n < gridSize; n++){
-				var obj = getBox(1,1,1);
-				obj.position.x = i;
-				obj.position.y = m;
-				obj.position.z = n;
-				obj.material.opacity = 0.0;
-				obj.name = 'Object_' + i.toString() + '_' + m.toString() + '_' + n.toString();
-				group.add(obj);
+				var obj = makeFreshBox(1,1,1, i, m, n);
+				obj.lifespan = 0;
+				obj.active = true;
+				gridList.push(obj);
 				}
 			}
 		}
-	return group;
-	}
+	return colorGeo(gridList);;
+}
 
 //search group for child object by child object's position
 function findBox(grid, position){
 	var xTest, yTest, zTest;
-	for (var i = 0; i < grid.children.length; i++){
-		xTest = grid.children[i].position.x;
-		yTest = grid.children[i].position.y;
-		zTest = grid.children[i].position.z;
-		if (xTest == position.x && yTest == position.y && zTest == position.z){
-			return grid.children[i];
-			}
+
+	for (var i = 0; i < grid.length; i++){
+		xTest = grid[i].position.x;
+		yTest = grid[i].position.y;
+		zTest = grid[i].position.z;
+
+		var check1 = xTest == position.x;
+		var check2 = yTest == position.y;
+		var check3 = zTest == position.z;
+		if (check1 && check2 && check3) {
+			return grid[i];
 		}
-	return undefined;
 	}
+
+	return undefined;
+}
 
 function randStartCoord(min, max, randomValues, grid){
 	var rX = 0;
@@ -121,9 +78,9 @@ function randStartCoord(min, max, randomValues, grid){
 		rY = getRandomInt(min, max);
 		rZ = getRandomInt(min, max);
 		vec = new THREE.Vector3(rX, rY, rZ);
-		var obj = findBox(grid,vec);
+		var obj = findBox(grid, vec);
 		if (obj != undefined){
-			obj.material.opacity = 1.0;
+			obj.visible = true;
 			onList.push(obj);
 			}
 		}
@@ -134,7 +91,7 @@ function neighborSum(grid, geo){
 	var neighborSum = 0;
 	var x, y, z;
 	var vec;
-	if (geo.material.opacity == 1.0){
+	if (geo.visible){
 		neighborSum -= 1;
 	}
 	//Check x and y coordinates of +1 and -1
@@ -146,106 +103,113 @@ function neighborSum(grid, geo){
 				z = geo.position.z + b;
 				vec = new THREE.Vector3(x,y,z);
 				var checkCur = findBox(grid, vec);
-				if (checkCur != undefined && checkCur.material.opacity == 1.0){
+				if (checkCur != undefined && checkCur.visible){
 					neighborSum += 1;
-					}
 				}
 			}
 		}
+	}
 	return neighborSum;
 	}
 
-function testPt(grid){
+function testPt(grid) {
 	var currentOff = [];
 	var currentOn = [];
-	//define next life of each cell
-	for (var i = 0; i < grid.children.length; i++){
-		var current = grid.children[i];
-		var nS = neighborSum(grid, current);
-		if (current.material.opacity == 1.0){
-			var on = true;
-		}else{
-			var on = false;
+	
+	// Define next life of each cell
+	for (var i = 0; i < grid.length; i++) {
+		var current = grid[i];
+		var nsum = neighborSum(grid, current);
+
+		var on = current.active;
+		var sweetSpotNbors = false;
+
+		if (4 < nsum < 8) sweetSpotNbors = true;
+		if (nsum == 6) on = true;
+
+		var makeOn = false;
+		if (on && sweetSpotNbors) {
+			makeOn = true;
+		}
+		if (current.lifespan > 2) {
+			makeOn = false;
 		}
 
-		if (on && nS < 5){
-			currentOff.push(current);
-		}else if (on && 5 <= nS <=7){
+		if (makeOn) {
 			currentOn.push(current);
-		}else if (on && nS > 7){
-			currentoff.push(current);
-		}else if (on == false && nS == 6){
-			currentOn.push(current);
-		}else {
+		} else {
 			currentOff.push(current);
-		}
-		}
-	//run visibility update
-	for (var m = 0; m < currentOff.length; m++){
-		if (currentOff[m].material.opacity > 0.0){
-			currentOff[m].material.opacity -= 0.25;
-			}
-		}
-	for (var n = 0; n < currentOn.length; n++){
-		currentOn[n].material.opacity = 1.0;
-		}
-	}
-
-function colorGeo(grid){
-	var obj = grid;
-	var mult = parseInt(255/(Math.cbrt(obj.children.length)))-2;
-	for (var i = 0; i < obj.children.length; i++){
-		var r = Math.abs(obj.children[i].position.x * mult);
-		var g = Math.abs(obj.children[i].position.y * mult);
-		var b = Math.abs(obj.children[i].position.z * mult);
-		var newColor =  new THREE.Color('rgb(' + r.toString() + ',' + g.toString() + ',' + b.toString() + ')');
-		obj.children[i].material.color = newColor;
 		}
 	}
 	
-//recieves mesh object, adds line outlines
-function addLines(scene){
-	for (var i = 0; i < scene.children[0].children.length; i++){
-		var geometry = scene.children[0].children[i].geometry;
-		var edges = new THREE.EdgesGeometry(geometry);
-		var color = new THREE.Color({color:'rgb(200,200,200)'});
-		var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({color: color}));
-		scene.add(line);
+	// Run visibility update
+	for (var m = 0; m < currentOff.length; m++){
+		if (currentOff[m].active) {
+			currentOff[m].active = false;
+			currentOff[m].lifespan = 0;
+
+			console.log('Removing...')
+			currentOff[m].parent.remove(currentOff[m]);
 		}
 	}
 
-function getPtLight(intensity){
-	var light = new THREE.PointLight(0xffffff, intensity);
-	return light;
+	for (var n = 0; n < currentOn.length; n++) {
+		currentOn[n].active = true;
+		currentOn[n].lifespan += 1;
 	}
 
-function getBox(w,h,d){
-	var geometry = new THREE.BoxGeometry(w,h,d);
-	var material = new THREE.MeshPhongMaterial({color: 'rgb(180,180,200)', wireframe: false});
-	material.flatShading = true;
-	material.shininess = 80;
-	material.opacity = 1;
-	material.transparent = true;
-	var mesh = new THREE.Mesh(geometry, material);
-	return mesh;
-	};
+	// Actually add as group to scene
+	var newGroup = new THREE.Group()
+	for (var n = 0; n < currentOn.length; n++) {
+		currentOn[n].active = true;
+		currentOn[n].lifespan += 1;
+		newGroup.add(currentOn[n])
+	}
+}
 
-function getSphere(r,w,h){
-	var geometry = new THREE.SphereGeometry(r,w,h);
-	var material = new THREE.MeshPhongMaterial({color: 'rgb(180,180,200)', wireframe: false});
-	material.shininess = 80;
+function colorGeo(grid) {
+	var mult = parseInt(255/(Math.cbrt(grid.length)))-2;
+
+	for (var i = 0; i < grid.length; i++){
+		var r = Math.abs(grid[i].position.x * mult);
+		var g = Math.abs(grid[i].position.y * mult);
+		var b = Math.abs(grid[i].position.z * mult);
+		var newColor =  new THREE.Color('rgb(' + r.toString() + ',' + g.toString() + ',' + b.toString() + ')');
+		
+		var box = grid[i];
+		box.material.color = newColor;
+		box.material.emissive = newColor;
+	}
+
+	return grid;
+}
+
+function makeFreshBox(i, m, n) {
+	var geometry = new THREE.BoxGeometry(1,1,1);
+
+	opts = {color: 'rgb(180,180,200)', wireframe: false}
+	var material = new THREE.MeshPhongMaterial(opts);
+
+	// Rendering options
+	material.flatShading = true;
 	material.opacity = 1;
-	material.transparent = true;
+	material.transparent = false;
+			
 	var mesh = new THREE.Mesh(geometry, material);
-	return mesh;
-	};
+	mesh.position.x = i;
+	mesh.position.y = m;
+	mesh.position.z = n;
+
+	mesh.visible = true;
+	mesh.name = `o_${i}_${m}_${n}`;
+
+	return mesh
+}
 
 function update(renderer, scene, camera, controls){
 	renderer.render(scene, camera);
 	controls.update();
-	requestAnimationFrame(function(){
+	requestAnimationFrame(function() {
 		update(renderer, scene, camera, controls);
-		})
-	};
-
-var scene = init();
+	})
+};
